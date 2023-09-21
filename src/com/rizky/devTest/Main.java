d8931da965b00f2b8b0380d51a1edd53824928a5
Rizky Subagja <rizky.subagja@btpn.com>
2023-09-21 19:31:17 +0700

    initial commit

---
+++ src/com/rizky/devTest/Main.java
@@ -0,0 +1,22 @@
+package com.rizky.devTest;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int size = scanner.nextInt();
+		
+		for(int row = 1; row <= size; row++) {
+			for(int numberOfSpace = size - row; numberOfSpace > 0; numberOfSpace--) {
+				System.out.print(' ');
+			}
+			
+			for(int numberOfSymbol = row; numberOfSymbol > 0; numberOfSymbol--) {
+				System.out.print('#');
+			}
+			
+			System.out.println();
+		}
+	}
+}
